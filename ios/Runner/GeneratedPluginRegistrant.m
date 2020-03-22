2749edb54e50f99497bd01cee6573090d5d9af10
Afnan Hassan <50952934+HassanAfnan@users.noreply.github.com>
2020-03-23 01:57:34 +0500

    Add files via upload

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<google_maps_flutter/FLTGoogleMapsPlugin.h>)
+#import <google_maps_flutter/FLTGoogleMapsPlugin.h>
+#else
+@import google_maps_flutter;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTGoogleMapsPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTGoogleMapsPlugin"]];
+}
+
+@end
---
+++ lib/main.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main(){
+  runApp(MapsDemo());
+}
+
+class MapsDemo extends StatefulWidget {
+  MapsDemo() : super();
+  @override
+   final String title = "Maps Demo";
+  _MapsDemoState createState() => _MapsDemoState();
+}
+
+class _MapsDemoState extends State<MapsDemo> {
+  Completer<GoogleMapController> _controller = Completer();
+  static const LatLng _center = const LatLng(45.521563, -122.677433);
+  final Set<Marker> _marker = {};
+  LatLng _lastMapPosition = _center;
+  MapType _currentMapType = MapType.normal;
+
+  static final CameraPosition _position1 = CameraPosition(
+    bearing: 192.833,
+    target: LatLng(45.531563,-122.677433),
+    tilt: 59.440,
+    zoom: 11.0
+  );
+
+  Future<void> _goToPosition() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_position1));
+  }
+
+  _onMapCreated(GoogleMapController controller)
+  {
+     _controller.complete(controller);
+  }
+
+  _onCameraMove(CameraPosition position)
+  {
+    _lastMapPosition = position.target;
+  }
+
+  _onMapTypeButtonPressed()
+  {
+    setState(() {
+      _currentMapType = _currentMapType == MapType.normal
+          ? MapType.satellite
+          : MapType.normal;
+    });
+  }
+
+  _onAddMarkerButtonPressed()
+  {
+    setState(() {
+      _marker.add(Marker(
+        markerId: MarkerId(_lastMapPosition.toString()),
+        position: _lastMapPosition,
+        infoWindow: InfoWindow(
+          title: 'This is a Title',
+          snippet: 'This is a snippet',
+        ),
+          icon: BitmapDescriptor.defaultMarker,
+      ),
+      );
+    });
+  }
+
+  Widget button(Function function,IconData icon)
+  {
+    return FloatingActionButton(
+      onPressed: function,
+      materialTapTargetSize: MaterialTapTargetSize.padded,
+      backgroundColor: Colors.blue,
+      child: Icon(
+        icon,
+        size: 36.0,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+          backgroundColor: Colors.blue,
+        ),
+        body: Stack(
+          children: <Widget>[
+            GoogleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(
+                target: _center,
+                zoom: 11.0,
+              ),
+              mapType: _currentMapType,
+              markers: _marker,
+              onCameraMove: _onCameraMove,
+            ),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Align(
+                alignment: Alignment.topRight,
+                child: Column(
+                  children: <Widget>[
+                    button(_onMapTypeButtonPressed, Icons.map),
+                    SizedBox(
+                      height: 16.0,
+                    ),
+                    button(_onAddMarkerButtonPressed, Icons.add_location),
+                    SizedBox(
+                      height: 16.0,
+                    ),
+                    button(_goToPosition, Icons.location_searching),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
